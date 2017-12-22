d243331baf73d4428ab9aa10a86667dc800629ab
chenchao <cqwzchenchao@163.com>
2017-12-22 16:12:25 +0800

    try fix bug

--- src/RDMACMSocket.cpp
+++ src/RDMACMSocket.cpp
@@ -10,7 +10,8 @@ RDMACMSocket::RDMACMSocket(struct rdma_cm_id *client_id)
 
 RDMACMSocket::~RDMACMSocket() {
   rdma_disconnect(this->client_id);
-  rdma_dereg_mr(this->verbs_mr);
+  //rdma_dereg_mr(this->verbs_mr);
+  ibv_dereg_mr(this->verbs_mr);
   rdma_destroy_ep(this->client_id);
   this->verbs_buf.free();
 }
@@ -60,8 +61,10 @@ RDMACMSocket *RDMACMSocket::connect(const HostAndPort &hp) {
 void RDMACMSocket::setup_verbs_buf() {
   this->verbs_buf = Buffer::allocate(PACKET_SIZE * PACKET_WINDOW_SIZE * 2);
 
-  this->verbs_mr = rdma_reg_msgs(this->client_id, this->verbs_buf.addr,
-                                 this->verbs_buf.size);
+  //this->verbs_mr = rdma_reg_msgs(this->client_id, this->verbs_buf.addr,
+                                 //this->verbs_buf.size);
+  this->verbs_mr = ibv_reg_mr(this->client_id->pd, this->verbs_buf.addr, this->verbs_buf.size,
+                          IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ);
   if (this->verbs_mr == NULL) {
     this->verbs_buf.free();
     rdma_destroy_ep(this->client_id);
